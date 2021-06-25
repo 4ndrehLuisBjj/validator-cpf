85cfa7f2b5a52a7462e8235defe1176d5ace158c
andre <djangosimples@gmail.com>
2021-06-25 15:10:59 -0300

    correção do código

--- js/main.js
+++ js/main.js
@@ -8,71 +8,52 @@ function validaCpf() {
 
 	var cpf = document.getElementById('number-cpf');
 
-	if (cpf.value.length !== 11) {
-		alert(cpf.value.length)
-	} else if (cpf.value == '11111111111' || cpf.value == '22222222222' || cpf == '33333333333' ||
+	if (cpf.value.length !== 11 || cpf.value == '11111111111' || cpf.value == '22222222222' || cpf == '33333333333' ||
 		cpf.value == '44444444444' || cpf.value == '55555555555' || cpf.value == '66666666666' ||
 		cpf.value == '77777777777' || cpf.value == '88888888888' || cpf.value == '99999999999' || 
 		cpf.value == '01234567890') {
-
+		
 		cpf.value = '';
 		response.textContent = 'CPF invalido!'
+		alerta.classList.remove('alert-success')
 		alerta.style.display = 'block';
 		cpf.focus();
 
-	} else {
+	} 
 
-		contador = 10;
-		resultado = 0;
-
-
-		for (x = 0; x < cpf.value.length; x++) {
-			if (contador == 1) {
-				break;
-			} else {
-				resultado += parseInt(cpf.value[x]) * contador;
-				contador -= 1
+		
+	resultado = 0;
 
 
-			}
-
-		}
-
-
-		digito_um = (resultado * 10) % 11
-
-		contador = 11;
-		resultado = 0;
-
-		if (digito_um == parseInt(cpf.value[9])) {
+	for (x = 0, contador = 10; contador >1; x++, contador--) {
 			
+		resultado += parseInt(cpf.value[x]) * contador;		
+	}
 
-			for (x = 0; x < cpf.value.length; x++) {
-				if (contador == 1) {
-					break;
-				} else {
-					
-					resultado += parseInt(cpf.value[x]) * contador;
-					contador -= 1
 
-				}
-			}
-			digito_dois = (resultado * 10) % 11;
+	digito_um = (resultado * 10) % 11
+	
+
+	resultado = 0;
+
+	if (digito_um == parseInt(cpf.value[9])) {
 			
+	for (x = 0, contador=11; contador>1; x++,contador--) {
+		resultado += parseInt(cpf.value[x]) * contador;
+	}
+
+	digito_dois = (resultado * 10) % 11;
 			
+	if (digito_dois == cpf.value[10]) {
 
-			if (digito_dois == cpf.value[10]) {
-
-				alerta.classList.remove('alert-danger');
-				alerta.classList.add('alert-success');
-				response.textContent = "CPF válido "+ cpf.value;
-				alerta.style.display = 'block';
-				cpf.value = '';
-				cpf.focus()
+		alerta.classList.remove('alert-danger');
+		alerta.classList.add('alert-success');
+			response.textContent = "CPF válido "+ cpf.value;
+			alerta.style.display = 'block';
+			cpf.value = '';
+			cpf.focus()
 				
-			}
-
-		} else {
+	} else {
 			
 			alerta.style.display = 'block';
 			response.textContent = 'CPF INVALIDO';
@@ -81,8 +62,8 @@ function validaCpf() {
 		}
 	}
 
-}
 
+}
 
 
 btnConsultar.addEventListener('click', validaCpf);
